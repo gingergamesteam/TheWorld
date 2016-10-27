612aacd3d8c00d4efa7f8dba9055eee655cbc1f2
avoinicaru <avoinicaru@ANDREIVOINICARU>
2016-10-27 18:15:15 +0300

    AppController fixed

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -2,13 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace TheWorld.wwwroot.Cotroolers.Web
+namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
