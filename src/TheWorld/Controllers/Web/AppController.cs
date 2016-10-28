6731ec9520a6d3669f7d2bbf6a29ece5a70fb17f
avoinicaru <avoinicaru@ANDREIVOINICARU>
2016-10-28 15:29:51 +0300

    05 MVC 6 Completing the Form

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,12 @@ namespace TheWorld.Controllers.Web
     public class AppController : Controller
     {
         private IMailService _mailService;
+        private IConfigurationRoot _config;
 
-        public AppController(IMailService mailService)
+        public AppController(IMailService mailService, IConfigurationRoot config)
         {
             _mailService = mailService;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -31,8 +34,18 @@ namespace TheWorld.Controllers.Web
         [HttpPost]
         public IActionResult Contact(ContactViewModel model)
         {
-            _mailService.SendMail("adei@aol.com", model.Email, "From TheWrold", model.Message);
+            if (model.Email.Contains("aol.com"))
+            {
+                ModelState.TryAddModelError("Email", "We dont't support AOL adresses");
+            }
 
+            if(ModelState.IsValid)
+            { 
+                _mailService.SendMail(_config["MailSettings:ToAdress"], model.Email, "From TheWrold", model.Message);
+
+                ModelState.Clear();
+                ViewBag.UserMessage = "Message Sent";
+            }
             return View();
         }
 
